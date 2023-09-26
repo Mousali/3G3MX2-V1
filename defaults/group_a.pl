37275ebd7ae7c1814471d32df3ed57ac83fb9cb1
Mousali <ammar.mousali@gmail.com>
2023-09-26 21:13:33 +1000

    added _default postfix

--- defaults/group_a.pl
+++ defaults/group_a.pl
@@ -1,180 +1,180 @@
 % Group A: Standard Function Parameters
 %% Basic operation settings
 
-a001("02").
-a201("02").
-a002("02").
-a202("02").
-a003("60.0").
-a203("60.0").
-a004("60.0").
-a204("60.0").
+a001_default("02").
+a201_default("02").
+a002_default("02").
+a202_default("02").
+a003_default("60.0").
+a203_default("60.0").
+a004_default("60.0").
+a204_default("60.0").
 
 %% Analog input
 
-a005("00").
-a011("0.00").
-a012("0.00").
-a013("0.").
-a014("100").
-a015("01").
-a016("8.").
+a005_default("00").
+a011_default("0.00").
+a012_default("0.00").
+a013_default("0.").
+a014_default("100").
+a015_default("01").
+a016_default("8.").
 
 %% DriveProgramming
 
-a017("00").
+a017_default("00").
 
 %% Multi-step speed, jogging
 
-a019("00").
-a020("6.00").
-a220("6.00").
-a021("0.00").
-a022("0.00").
-a023("0.00").
-a024("0.00").
-a025("0.00").
-a026("0.00").
-a027("0.00").
-a028("0.00").
-a029("0.00").
-a030("0.00").
-a031("0.00").
-a032("0.00").
-a033("0.00").
-a034("0.00").
-a035("0.00").
-a038("6.00").
-a039("04").
+a019_default("00").
+a020_default("6.00").
+a220_default("6.00").
+a021_default("0.00").
+a022_default("0.00").
+a023_default("0.00").
+a024_default("0.00").
+a025_default("0.00").
+a026_default("0.00").
+a027_default("0.00").
+a028_default("0.00").
+a029_default("0.00").
+a030_default("0.00").
+a031_default("0.00").
+a032_default("0.00").
+a033_default("0.00").
+a034_default("0.00").
+a035_default("0.00").
+a038_default("6.00").
+a039_default("04").
 
 %% V/f characteristics
 
-a041("01").
-a241("01").
-a042("1.0").
-a241("1.0").
-a043("5.0").
-a243("5.0").
-a044("00").
-a244("00").
-a045("100.").
-a245("100.").
-a046("100.").
-a246("100.").
-a047("0.").
-a247("0.").
+a041_default("01").
+a241_default("01").
+a042_default("1.0").
+a241_default("1.0").
+a043_default("5.0").
+a243_default("5.0").
+a044_default("00").
+a244_default("00").
+a045_default("100.").
+a245_default("100.").
+a046_default("100.").
+a246_default("100.").
+a047_default("0.").
+a247_default("0.").
 
 %% DC injection braking
 
-a051("00").
-a052("0.50").
-a053("0.0").
-a054("50.").
-a055("0.5").
-a056("01").
-a057("0.").
-a058("0.0").
-a059("5.0").
+a051_default("00").
+a052_default("0.50").
+a053_default("0.0").
+a054_default("50.").
+a055_default("0.5").
+a056_default("01").
+a057_default("0.").
+a058_default("0.0").
+a059_default("5.0").
 
 %% Limit, jump
 
-a061("0.00").
-a261("0.00").
-a062("0.00").
-a262("0.00").
-a063("0.00").
-a064("0.50").
-a065("0.00").
-a066("0.50").
-a067("0.00").
-a068("0.50").
+a061_default("0.00").
+a261_default("0.00").
+a062_default("0.00").
+a262_default("0.00").
+a063_default("0.00").
+a064_default("0.50").
+a065_default("0.00").
+a066_default("0.50").
+a067_default("0.00").
+a068_default("0.50").
 
 %% Acceleration stop
 
-a069("0.00").
-a070("0.0").
+a069_default("0.00").
+a070_default("0.0").
 
 %% PID Braking
 
-a071("00").
-a072("1.00").
-a073("1.0").
-a074("0.00").
-a075("1.00").
-a076("00").
-a077("00").
-a078("0.0").
-a079("00").
+a071_default("00").
+a072_default("1.00").
+a073_default("1.0").
+a074_default("0.00").
+a075_default("1.00").
+a076_default("00").
+a077_default("00").
+a078_default("0.0").
+a079_default("00").
 
 %% ARV
 
-a081("02").
-a281("02").
-a082("200").
-a282("20").
-a083("0.300").
-a084("100.").
+a081_default("02").
+a281_default("02").
+a082_default("200").
+a282_default("20").
+a083_default("0.300").
+a084_default("100.").
 
 %% Operation mode, acceleration/deceleration function
 
-a085("00").
-a086("50.0").
-a092("10.0").
-a292("10.0").
-a093("10.0").
-a293("10.0").
-a094("00").
-a294("00").
-a095("0.00").
-a295("0.00").
-a096("0.00").
-a296("0.00").
-a097("01").
-a098("01").
+a085_default("00").
+a086_default("50.0").
+a092_default("10.0").
+a292_default("10.0").
+a093_default("10.0").
+a293_default("10.0").
+a094_default("00").
+a294_default("00").
+a095_default("0.00").
+a295_default("0.00").
+a096_default("0.00").
+a296_default("0.00").
+a097_default("01").
+a098_default("01").
 
 
 %% Frequency adjustment
 
-a101("0.00").
-a102("0.00").
-a103("20").
-a104("100").
-a105("00").
+a101_default("0.00").
+a102_default("0.00").
+a103_default("20").
+a104_default("100").
+a105_default("00").
 
 %% Acceleration/Deceleration
 
-a131("02").
-a132("02").
+a131_default("02").
+a132_default("02").
 
 %% Calculation frequency
 
-a141("02").
-a142("03").
-a143("00").
-a145("0.00").
-a146("00").
+a141_default("02").
+a142_default("03").
+a143_default("00").
+a145_default("0.00").
+a146_default("00").
 
 %% Acceleration/Deceleration
 
-a150("10.").
-a151("10.").
-a152("10.").
-a153("10.").
+a150_default("10.").
+a151_default("10.").
+a152_default("10.").
+a153_default("10.").
 
 %% Deceleration stop
 
-a154("0.00").
-a155("0.0").
+a154_default("0.00").
+a155_default("0.0").
 
 %% PID
 
-a156("0.00").
-a157("0.0").
+a156_default("0.00").
+a157_default("0.0").
 
 %% Frequency adjustment
 
-a161("0.00").
-a162("0.00").
-a163("0.").
-a164("100.").
-a165("01").
+a161_default("0.00").
+a162_default("0.00").
+a163_default("0.").
+a164_default("100.").
+a165_default("01").
