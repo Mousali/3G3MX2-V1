759c01b2aae08017681c68c861bab456ada37e3a
Mousali <ammar.mousali@gmail.com>
2023-09-27 11:20:04 +1000

    added group h defaults

---
+++ defaults/group_h.pl
@@ -0,0 +1,64 @@
+h001_default{"00"}.
+h002_default{"00"}.
+h202_default{"00"}.
+% h003_default{""}.
+% h203_default{""}.
+h004_default{"4"}.
+h204_default{"4"}.
+h005_default{"100."}.
+h205_default{"100."}.
+h006_default{"100."}.
+h206_default{"100."}.
+
+% h020_default{""}.
+% h220_default{""}.
+% h021_default{""}.
+% h221_default{""}.
+% h022_default{""}.
+% h222_default{""}.
+% h023_default{""}.
+% h223_default{""}.
+% h024_default{""}.
+% h224_default{""}.
+
+% h030_default{""}.
+% h230_default{""}.
+% h031_default{""}.
+% h231_default{""}.
+% h032_default{""}.
+% h232_default{""}.
+% h033_default{""}.
+% h233_default{""}.
+% h034_default{""}.
+% h234_default{""}.
+
+h050_default{"0.20"}.
+h051_default{"2."}.
+
+% h102_default{""}.
+% h103_default{""}.
+% h104_default{""}.
+% h105_default{""}.
+% h106_default{""}.
+% h107_default{""}.
+% h108_default{""}.
+% h109_default{""}.
+% h110_default{""}.
+
+% h111_default{""}.
+% h112_default{""}.
+% h113_default{""}.
+
+h116_default{"100"}.
+h117_default{"70.00"}.
+h118_default{"1.00"}.
+h119_default{"100"}.
+
+h121_default{"8.0"}.
+h122_default{"10.00"}.
+h123_default{"00"}.
+
+h131_default{"10"}.
+h132_default{"10"}.
+h133_default{"30"}.
+h134_default{"100"}.
