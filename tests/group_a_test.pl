cab58aeae13e9d922708210ffe1a064dbb9f70f9
Mousali <ammar.mousali@gmail.com>
2025-06-01 21:50:04 +1000

    changed test case to take spec dict instead of spec yaml file

--- tests/group_a_test.pl
+++ tests/group_a_test.pl
@@ -2,16 +2,26 @@
 
 :- ["../param/group_a"].
 
-load_spec(SpecFilename):-
-    yaml_read(SpecFilename, Str),
-    b_setval(spec, Str).
-
+load_spec(D):-
+    b_setval(spec, D).
 
 test(
-        "a001 is ok", 
-        [ 
-            setup(load_spec('test_spec.yaml'))
-        ]
-    ):- a001(01,calculated).
+    "a001 is ok", 
+    [
+        setup(
+            load_spec(
+dict{
+    motor:dict{
+        '1st':
+        dict{
+            speed_reference:"voltage (FV)"
+            }
+        }
+}
+            )
+        )
+    ]
+):- 
+    a001(01,calculated).
 
 :- end_tests(group_a).
